c7ba788f6dbcd27492d6d4cdf64a166ea9f64b0d
RickyAdriell <53133228+codecplyre@users.noreply.github.com>
2023-01-25 18:53:49 +0000

    init flags for

--- main.go
+++ main.go
@@ -1,7 +1,53 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+)
+
+var (
+	B         bool
+	O         string
+	P         string
+	I         bool
+	RateLimit string
+	Mirror    bool
+	Reject    string
+	Exclude   string
+)
+
+func init() {
+	flag.BoolVar(&B, "B", false, "Output in wget-log")
+	flag.StringVar(&O, "O", "", "Specify download filename")
+	flag.StringVar(&P, "P", "./", "Specify download directory")
+	flag.BoolVar(&I, "i", false, "Download multiple files")
+	flag.StringVar(&RateLimit, "rate-limit", "0", "The rate limit in k = KB/s or  M = MB/s")
+	flag.BoolVar(&Mirror, "mirror", false, "Mirror the whole site")
+	flag.StringVar(&Reject, "reject", "", "Reject files")
+	flag.StringVar(&Exclude, "exclude", "", "Exclude directory")
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	flag.Parse()
+	if B {
+		fmt.Println("Output in wget-log is enabled")
+	} else {
+		fmt.Println("Output in wget-log is disabled")
+	}
+	fmt.Println("filename:", O)
+	fmt.Println("directory:", P)
+	if I {
+		fmt.Println("Download multiple files is enabled")
+	} else {
+		fmt.Println("Download multiple files is disabled")
+	}
+	fmt.Println("Rate Limit: ", RateLimit)
+	if Mirror {
+		fmt.Println("Mirror the whole site is enabled")
+	} else {
+		fmt.Println("Mirror the whole site is disabled")
+	}
+	fmt.Println("Reject:", Reject)
+	fmt.Println("Exclude:", Exclude)
+	fmt.Println("URL:", flag.Arg(0))
 }
