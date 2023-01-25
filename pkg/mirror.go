9b46136d82d0018bf3cf6755a5f90445793d9e37
keivon <keivon.991@gmail.com>
2023-01-25 22:07:39 +0000

    FindStylesheet

---
+++ pkg/mirror.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"strings"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+
+
+func FindStylesheet( doc *goquery.Document) {
+	doc.Find("link").Each(func(i int, s *goquery.Selection) {
+		imgSrc, exists := s.Attr("href")
+		if !exists {
+			fmt.Println("link href not found")
+			return
+		} else {
+			if strings.Contains(imgSrc, ".css") &&  strings.Contains(imgSrc, "https://"){
+
+				//s.SetAttr("href", "keivon") 
+			} else if strings.Contains(imgSrc, ".css") {
+
+			}
+			fmt.Println(imgSrc)
+			//fmt.Println(doc.Html())
+		}
+	})
+}
+
+
+
+
+func mirror(url string) {
+	res, err := http.Get("https://jonathanmh.com/web-scraping-golang-goquery/")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer res.Body.Close()
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+
+
+	doc.Find("img").Each(func(i int, s *goquery.Selection) {
+		imgSrc, exists := s.Attr("src")
+		if !exists {
+			fmt.Println("Image src not found")
+			return
+		} else {
+			if strings.Contains(imgSrc, ".css") &&  strings.Contains(imgSrc, "https://"){
+
+				//s.SetAttr("href", "keivon") 
+			} else if strings.Contains(imgSrc, ".css") {
+
+			}
+			fmt.Println(imgSrc)
+			//fmt.Println(doc.Html())
+		}
+	})
+
+	
+}
